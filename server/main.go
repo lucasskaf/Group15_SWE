e36154a532d93780b7e0eb3be3e4f7c0d05f78db
Elliot Liu <elliot.liu21@gmail.com>
2023-02-06 21:16:34 -0500

    Fixed broken imports for Gorm

---
+++ server/main.go
@@ -0,0 +1,33 @@
+package main
+
+//imports commented out to avoid generating errors for unused
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+	/*
+			"gorm.io/driver/sqlite"
+			"net/http"
+		    "github.com/gin-gonic/gin"
+	*/)
+
+type movie struct {
+	title   string
+	runtime float32
+}
+
+type user struct {
+	gorm.Model
+	username  string
+	password  string
+	watchlist []movie
+	genres    []string
+	rating    float32
+	providers []string
+}
+
+func main() {
+	fmt.Println("test")
+
+}
