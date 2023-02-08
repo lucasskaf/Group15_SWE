98af041c98bbf542dcfa8f5dc0bfcfb55f8c08ed
Lucas Skaf <skaf.lucas@ufl.edu>
2023-02-08 16:52:45 -0500

    Update colors, text in login-register and serve.sh

--- serve.sh
+++ serve.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
+!/bin/sh
 
 ng serve &
-<<<<<<< HEAD
-gin --port 4201 --path . --build ./src/server/ --i --all &
-=======
+
 gin --port 4200 --path . --build ./src/server/ --i --all &
->>>>>>> origin/Lucas
 
 wait
