24bf1719eb1ec4f96c4ec60f61ec926ba5b2a910
Lucas Skaf <skaf.lucas@ufl.edu>
2023-02-27 22:00:16 -0500

    Modify login-register to work with login (nav-bar)

--- src/app/login-register/login-register.component.ts
+++ src/app/login-register/login-register.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
-import { loginInteraction, closeLogin } from './login-register'
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { loginInteraction } from './login-register'
 
 @Component({
-  selector: 'app-login-register',
+  selector: 'bb-login-register',
   templateUrl: './login-register.component.html',
   styleUrls: ['./login-register.component.css']
 })
 export class LoginRegisterComponent {
+  @Output() isClosed = new EventEmitter()
+  @Input() isOpen : boolean = false
   
   ngOnInit(): void {
     const loginAnimation = loginInteraction()
-    const closeFunctionality = closeLogin()
   }
 
+  toogleLogin() {
+    this.isOpen = !this.isOpen
+    this.isClosed.emit(this.isOpen)
+  }
 }
