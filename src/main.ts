851195d7814327b4995e8dba96c23abecc0e5e50
ErnestoRoja <ernestoroja@ufl.edu>
2023-03-01 22:36:00 -0500

    changed .json to support cypress

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment.development';
-
-
-if (environment.production) {
-  enableProdMode();
-}
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
