a15d32c59e832f55dc700182de4f9d7173a4142d
Lucas Skaf <skaf.lucas@ufl.edu>
2023-03-25 18:10:39 -0400

    Add movie generator to config files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { MatIconModule } from '@angular/material/icon'
 import {MatButtonModule} from '@angular/material/button';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { UserAuthModule } from './user-auth/user-auth.module';
+import { MovieGeneratorComponent } from './movie-generator/movie-generator.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MovieGeneratorComponent,
   ],
   imports: [
     BrowserModule,
