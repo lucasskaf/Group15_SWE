c5066ac07d4cc421b04fffc7109d21fbdefb486e
ErnestoRoja <ernestoroja@ufl.edu>
2023-03-29 14:50:55 -0400

    implemented form validator for title, actor, genre, rating, and runtime

--- src/app/profile-page/movie-form/movie-form.component.ts
+++ src/app/profile-page/movie-form/movie-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { MovieService } from '../../services/movie-service';
 import { MovieComponent } from '../../common/movie/movie.component';
 
@@ -15,16 +15,40 @@ export class MovieFormComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      title: new FormControl(''),
-      actor: new FormControl(''),
-      genre: new FormControl(''),
-      rating: new FormControl(''),
-      runtime: new FormControl('')
+      title: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      actor: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[a-zA-Z\\s]+')
+      ])),
+      genre: new FormControl('', Validators.compose([
+        Validators.required
+      ])),
+      rating: new FormControl('', Validators.compose([
+        Validators.required, 
+        this.ratingValidator
+      ])),
+      runtime: new FormControl('', Validators.compose([
+        Validators.required,
+        this.ratingValidator
+      ]))
     });
   }
 
+  ratingValidator (control: FormControl) {
+    const rating = control.value;
+    const maxRating = 999;
+
+    if (!/^\d{1,3}$/.test(rating) || parseInt(rating, 10) > maxRating) {
+      return { rating: true };
+    }
+    return null;
+  }
+
   onSubmit(movie: MovieComponent) {
-    console.log(movie);
+    // console.log(movie);
     this.movieService.addWatchedMovie(movie);
     this.form.reset();
   }
