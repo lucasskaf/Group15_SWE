99950ac58af6d4dea159c4f1863acbf5be2d1a30
unknown <ernestoroja@ufl.edu>
2023-04-04 21:31:03 -0400

    adjustments from main

--- src/app/user-auth/login-register/login-register.component.spec.ts
+++ src/app/user-auth/login-register/login-register.component.spec.ts
@@ -1,37 +1,31 @@
 // import { ComponentFixture, TestBed } from "@angular/core/testing";
-// import { ReactiveFormsModule } from "@angular/forms";
-// import { MatIconModule } from "@angular/material/icon";
-// import { LoginRegisterService } from "src/app/services/login-register.service";
 // import { LoginRegisterComponent } from "./login-register.component";
 
-// // describe("LoginRegisterComponent", () => {
-// //   let component: LoginRegisterComponent;
-// //   let fixture: ComponentFixture<LoginRegisterComponent>;
+// describe("LoginRegisterComponent", () => {
+//   let component: LoginRegisterComponent;
+//   let fixture: ComponentFixture<LoginRegisterComponent>;
 
 //   beforeEach(async () => {
 //     await TestBed.configureTestingModule({
-//       imports: [HttpClientTestingModule, MatIconModule, ReactiveFormsModule],
 //       declarations: [LoginRegisterComponent],
-//       providers: [LoginRegisterService]
 //     }).compileComponents();
 //   });
 
-// //   beforeEach(() => {
-// //     fixture = TestBed.createComponent(LoginRegisterComponent);
-// //     component = fixture.componentInstance;
-// //     fixture.detectChanges();
-// //   });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(LoginRegisterComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
 //   it('should emit closure', () => {
 //     const fixture = TestBed.createComponent(LoginRegisterComponent);
 //     // spy on event emitter
-//     const component = fixture.componentInstance;
-//     component.toogleLogin()
-//     let spy = spyOn(component.isClosed, 'emit')
+//     const component = fixture.componentInstance; 
+//     spyOn(component.isClosed, 'emit');
 
 //     // trigger the click
 //     const nativeElement = fixture.nativeElement;
-//     const button = nativeElement.querySelector('#closeButton');
+//     const button = nativeElement.querySelector('button#closeButton');
 //     button.dispatchEvent(new Event('click'));
 
 //     fixture.detectChanges();
