032c65dd21b799876c8088258295a2bbb7bd0b6c
Lucas Skaf <skaf.lucas@ufl.edu>
2023-04-09 17:17:16 -0400

    Add global fole for emmiters

--- src/app/emitters/emmiters.ts
+++ src/app/emitters/emmiters.ts
@@ -3,4 +3,5 @@ import { EventEmitter } from "@angular/core";
 export class Emmiters {
     static authEmmiter = new EventEmitter<boolean>()
     static userData = new EventEmitter<string>()
+    static isLoginOpen = new EventEmitter<boolean>()
 }
