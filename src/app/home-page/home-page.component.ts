8713a96eaad506679c20fd970082a1d38bdb5ac9
Lucas Skaf <skaf.lucas@ufl.edu>
2023-04-12 22:50:41 -0400

    Set up home page with carousel [layout]

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Emmiters } from '../emitters/emmiters';
 import { MovieGeneratorService } from '../services/movie-generator.service';
+import { Movie } from '../user-auth/user';
 
 
 @Component({
@@ -13,6 +14,44 @@ export class HomePageComponent implements OnInit {
   isAuthenticated
   username = ""
   message = 'Home Page'
+  movieList: Movie[] = [
+    {
+      title: "Movie1",
+      release_date: "01-01-2023",
+      poster_path: "https://cdn.shopify.com/s/files/1/1057/4964/products/Avengers-Endgame-Vintage-Movie-Poster-Original-1-Sheet-27x41.jpg?v=1670821335",
+      user_rating: 10
+    },
+    {
+      title: "Movie2",
+      release_date: "01-01-2023",
+      poster_path: "https://cdn.shopify.com/s/files/1/1057/4964/products/Avengers-Endgame-Vintage-Movie-Poster-Original-1-Sheet-27x41.jpg?v=1670821335",
+      user_rating: 10
+    },
+    {
+      title: "Movie3",
+      release_date: "01-01-2023",
+      poster_path: "https://cdn.shopify.com/s/files/1/1057/4964/products/Avengers-Endgame-Vintage-Movie-Poster-Original-1-Sheet-27x41.jpg?v=1670821335",
+      user_rating: 10
+    },
+    {
+      title: "Movie4",
+      release_date: "01-01-2023",
+      poster_path: "https://cdn.shopify.com/s/files/1/1057/4964/products/Avengers-Endgame-Vintage-Movie-Poster-Original-1-Sheet-27x41.jpg?v=1670821335",
+      user_rating: 10
+    },
+    {
+      title: "Movie5",
+      release_date: "01-01-2023",
+      poster_path: "https://cdn.shopify.com/s/files/1/1057/4964/products/Avengers-Endgame-Vintage-Movie-Poster-Original-1-Sheet-27x41.jpg?v=1670821335",
+      user_rating: 10
+    },
+    {
+      title: "Movie6",
+      release_date: "01-01-2023",
+      poster_path: "https://cdn.shopify.com/s/files/1/1057/4964/products/Avengers-Endgame-Vintage-Movie-Poster-Original-1-Sheet-27x41.jpg?v=1670821335",
+      user_rating: 10
+    }
+  ]
 
   constructor(
     private movieGeneratorService: MovieGeneratorService
