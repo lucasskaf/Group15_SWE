7063126e410b99a12bf50973f15645b7dca13664
Lucas Skaf <skaf.lucas@ufl.edu>
2023-04-12 22:50:08 -0400

    Add Carousel to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,6 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { UserAuthModule } from './user-auth/user-auth.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MatIconModule } from '@angular/material/icon'
-import { MatCardModule } from '@angular/material/card';
-import { Routes } from '@angular/router';
 import { NgToastModule } from 'ng-angular-popup';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -20,6 +18,7 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatSelectModule } from '@angular/material/select'
 import { MatSliderModule } from '@angular/material/slider'
 import {MatInputModule} from '@angular/material/input';
+import { CarouselModule } from 'primeng/carousel'
 
 @NgModule({
   declarations: [
@@ -44,7 +43,8 @@ import {MatInputModule} from '@angular/material/input';
     MatSelectModule,
     MatSliderModule,
     FormsModule,
-    MatInputModule
+    MatInputModule,
+    CarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
