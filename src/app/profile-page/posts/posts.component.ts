c517ceb4e521ff81951d58e1ebbe71624d00f0b1
unknown <ernestoroja@ufl.edu>
2023-04-19 22:15:48 -0400

    implemented post css and adjusted watched and post section layouts

--- src/app/profile-page/posts/posts.component.ts
+++ src/app/profile-page/posts/posts.component.ts
@@ -1,30 +1,34 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { PostService } from '../../services/post-service';
-import { PostComponent } from 'src/app/common/post/post.component';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { moviePosts } from 'src/app/user-auth/user';
+import { Emmiters } from 'src/app/emitters/emmiters';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   @Output() openPostAddPopup = new EventEmitter<boolean>();
   @Input() addPostPopupStatus: boolean = false;
   
-  posts: PostComponent[] = [];
+  posts: moviePosts[] = [];
 
-  constructor(private postService: PostService) {}
-
+  constructor() {}
+  
   ngOnInit() {
-    this.posts = this.postService.getPostsMade();
+    this.posts = Emmiters.userPosts;
   }
 
-  onPostDelete(post: PostComponent) {
-    this.postService.deletePost(post);
-  }
+  // ngOnInit() {
+  //   this.posts = this.postService.getPostsMade();
+  // }
 
-  public onPostAddClick(): void {
-    this.openPostAddPopup.emit(!this.addPostPopupStatus);
-    this.addPostPopupStatus = !this.addPostPopupStatus;
-  }
+  // onPostDelete(post: PostComponent) {
+  //   this.postService.deletePost(post);
+  // }
+
+  // public onPostAddClick(): void {
+  //   this.openPostAddPopup.emit(!this.addPostPopupStatus);
+  //   this.addPostPopupStatus = !this.addPostPopupStatus;
+  // }
 }
